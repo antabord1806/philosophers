a7bba9ff77128accf135668d2f884304a02d4873
antabord <antabord@student.42lisboa.com>
2025-11-12 14:52:10 +0000

    mutex_n_threads_create

--- philo/include/structs.h
+++ philo/include/structs.h
@@ -3,11 +3,24 @@
 
 #include "philo.h"
 
+#define WAITING_0   0 //0 mutexes
+#define WAITING_1   1 //1 mutex
+#define RUNNING     2 //2 mutexes
+
 typedef struct s_mutexes
 {
     pthread_mutex_t m;
     pthread_t owner;
-    bool locked;
+    bool locked_status;
 }   mutex_tracker_t;
+
+typedef struct s_thread_info
+{
+    pthread_t   th;
+    mutex_tracker_t *mutex;
+    int     mutex1_id;
+    int     mutex2_id;
+    int     status;
+}   thread_info_t;
 
 #endif
--- philo/src/threads.c
+++ philo/src/threads.c
@@ -1,36 +1,74 @@
 #include "../include/structs.h"
 #include "../include/philo.h"
 
+static void    *eating_spaggetti(void *args)
+{
+    int i;
+    thread_info_t *status;
+    mutex_tracker_t *mutex;
+    pthread_t thread;
+    i = -1;
 
-int     thread_creator(int n_forks)
+    status = (thread_info_t *)args;
+    mutex = status->mutex;
+    thread = status->th;
+    status->status = thread;
+    while (1)
+    {
+        if (mutex[++i].locked_status == true)
+            ;
+        else
+        {
+            mutex[i].owner = thread;
+            mutex[i].locked_status = true;
+            if (mutex[i++].locked_status == true)
+                status->status = WAITING_1;
+            else
+                status->status = RUNNING;
+        }
+        if (status->status == WAITING_1 || status->status == RUNNING)
+            return;
+    }
+    return (NULL);
+}
+
+static thread_info_t    starter_args(mutex_tracker_t *mutex, pthread_t thread, int i)
+{
+    thread_info_t *args;
+
+    args[i].mutex = mutex;
+    args[i].mutex1_id = -1;
+    args[i].mutex2_id = -1;
+    args[i].th = thread;
+    args[i].status = -1;
+    return (args);
+}
+int     thread_creator(mutex_tracker_t *mutex, thread_info_t *threads, thread_info_t *args, size_t n_forks)
 {
     pthread_t *th;
-    mutex_tracker_t *mutex;
+    thread_info_t args[n_forks];
     int i;
 
-    i = 0;
-    mutex = malloc(n_forks * sizeof(mutex_tracker_t));
-    if (!mutex)
-        return (0);
+    i = -1;
     while (i++ < n_forks)
     {
         pthread_mutex_init(&mutex[i].m, NULL);
-        mutex[i]. locked = FALSE;
-        i++;
-        printf("mutex_criada: %d locked_status: %d\n", mutex[i].m, mutex[i].locked);
+        mutex[i].locked_status = false;
+        printf("mutex_criada: %p locked_status: %d\n", (void *)&mutex[i].m, (int)mutex[i].locked_status);
+        args[i] = starter_args(mutex, th[i], i);
     }
     i = -1;
     th = malloc(n_forks * sizeof(pthread_t));
-    if (!th)
+    if (!th || !args)
         return (0);
     while (i++ < n_forks)
     {
-        if (pthread_create(th[i], NULL, timer, NULL) != 0)
-            return (printf("Errso na criação de thread\n"), 0);
-        printf("thread_criada: %d\n", th[i]);
+        if (pthread_create(th[i], NULL, eating_spaggetti, &args[i]) != 0)
+            return (printf("Errso na criação de thread\n"), free(th), 0);
+        printf("thread_criada: %p\n", (void *)th[i]);
     }
     th[i] = '\0';
-    i = -1;
+    free(th);
     return (0);
 }
 
@@ -38,6 +76,7 @@ int     thread_creator(int n_forks)
 static long long	ft_atol(char *str)
 {
 	long long	result;
+    long long   total;
 	int		sign;
 
 	result = 0;
@@ -55,12 +94,29 @@ static long long	ft_atol(char *str)
 		result = result * 10 + (*str - '0');
 		str++;
 	}
-	return (result * sign);
+    total = result * sign;
+    if (total < 0)
+        return (0);
+    else
+	    return (total);
 }
 
 int     main(int argc, char *argv[])
 {
+    mutex_tracker_t *mutex;
+    thread_info_t *threads;
+    long long n_forks;
+
     if (argc != 5)
+    return (0);
+    n_forks = ft_atol(argv[1]);
+    mutex = malloc(n_forks * sizeof(mutex_tracker_t));
+    if (!mutex)
         return (0);
-    thread_creator(ft_atol(argv[1]));
+    threads = malloc(n_forks * sizeof(thread_info_t));
+    if (!threads)
+        return (0);
+    thread_creator(mutex, threads, (size_t)n_forks);
+    free(mutex);
+    free(threads);
 }
